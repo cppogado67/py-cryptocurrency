7048934f65710a8950cccac6c0445ad59e2bcc20
claro <c_ppogado67@yahoo.com>
2026-09-25 11:22:36 +0800

    005

--- app/test_main.py
+++ app/test_main.py
@@ -4,33 +4,36 @@ from app import main
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_buy_more_cryptocurrency(mock_prediction):
-	mock_prediction.return_value = 106
+def test_buy_more_cryptocurrency(mock_prediction: object) -> None:
+    mock_prediction.return_value = 106
 
-	result = main.cryptocurrency_action(100)
+    result = main.cryptocurrency_action(100)
 
-	assert result == "Buy more cryptocurrency"
+    assert result == "Buy more cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_sell_all_cryptocurrency(mock_prediction):
-	mock_prediction.return_value = 94
+def test_sell_all_cryptocurrency(mock_prediction: object) -> None:
+    mock_prediction.return_value = 94
 
-	result = main.cryptocurrency_action(100)
+    result = main.cryptocurrency_action(100)
 
-	assert result == "Sell all your cryptocurrency"
+    assert result == "Sell all your cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction", return_value=95)
-def test_do_nothing_at_five_percent_lower_boundary(mock_prediction):
-	result = main.cryptocurrency_action(100)
+def test_do_nothing_at_five_percent_lower_boundary(
+    mock_prediction: object,
+) -> None:
+    result = main.cryptocurrency_action(100)
 
-	assert result == "Do nothing"
+    assert result == "Do nothing"
 
 
 @patch("app.main.get_exchange_rate_prediction", return_value=105)
-def test_do_nothing_at_five_percent_upper_boundary(mock_prediction):
-	result = main.cryptocurrency_action(100)
+def test_do_nothing_at_five_percent_upper_boundary(
+    mock_prediction: object,
+) -> None:
+    result = main.cryptocurrency_action(100)
 
-
-	assert result == "Do nothing"
+    assert result == "Do nothing"
