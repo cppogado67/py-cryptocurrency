a4f3646a8d576fc820c26fc29570ccb952f5e7f3
claro <c_ppogado67@yahoo.com>
2026-09-25 11:20:02 +0800

    003

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,36 @@
-# write your code here
+from unittest.mock import patch
+
+from app import main
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_buy_more_cryptocurrency(mock_prediction):
+	mock_prediction.return_value = 106
+
+	result = main.cryptocurrency_action(100)
+
+	assert result == "Buy more cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_sell_all_cryptocurrency(mock_prediction):
+	mock_prediction.return_value = 94
+
+	result = main.cryptocurrency_action(100)
+
+	assert result == "Sell all your cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction", return_value=95)
+def test_do_nothing_at_five_percent_lower_boundary(mock_prediction):
+	result = main.cryptocurrency_action(100)
+
+	assert result == "Do nothing"
+
+
+@patch("app.main.get_exchange_rate_prediction", return_value=105)
+def test_do_nothing_at_five_percent_upper_boundary(mock_prediction):
+	result = main.cryptocurrency_action(100)
+
+
+	assert result == "Do nothing"
